fe7c632a9360bfb2814d5dd9524234794c390d5a
Roubaud Philémon <roubaudphilemon@gmail.com>
2019-03-29 09:35:40 +0100

    PicoloButton squeletton

--- app/src/main/java/com/example/picoloid/source/activity/MainActivity.java
+++ app/src/main/java/com/example/picoloid/source/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.picoloid.source.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.picoloid.R;
 
@@ -11,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.openPageButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ii=new Intent(MainActivity.this, PageActivityEditor.class);
+                ii.putExtra("name", "pd");
+                startActivity(ii);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/picoloid/source/activity/PageActivityEditor.java
+++ app/src/main/java/com/example/picoloid/source/activity/PageActivityEditor.java
@@ -1,9 +1,14 @@
 package com.example.picoloid.source.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.RelativeLayout;
 
 import com.example.picoloid.R;
+import com.example.picoloid.source.model.PicoloButton;
+import com.example.picoloid.source.view.PicoloButtonView;
 
 public class PageActivityEditor extends AppCompatActivity {
 
@@ -11,5 +16,18 @@ public class PageActivityEditor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_editor);
+
+        Intent iin= getIntent();
+        Bundle b = iin.getExtras();
+
+        String j =(String) b.get("name");
+
+
+        PicoloButton buttonData = new PicoloButton();
+        buttonData.setTitle(j);
+
+        PicoloButtonView button = new PicoloButtonView(getApplicationContext(),buttonData);
+        RelativeLayout rl = (RelativeLayout)findViewById(R.id.editor_relative_layout);
+        rl.addView(button);
     }
 }
