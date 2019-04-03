9b7eac486b85ada990128f99bc11e1e0f63d3185
Roubaud Philémon <roubaudphilemon@gmail.com>
2019-04-03 14:42:28 +0200

    page loader from JSON working

--- app/src/main/java/com/example/picoloid/source/service/PicoloBookService.java
+++ app/src/main/java/com/example/picoloid/source/service/PicoloBookService.java
@@ -1,10 +1,14 @@
 package com.example.picoloid.source.service;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.example.picoloid.source.model.PicoloBook;
 
 public class PicoloBookService {
+
+    private static final String TAG = "PicoloBookService";
+
     private static class PicoloBookServiceHolder {
         private static final PicoloBookService INSTANCE = new PicoloBookService();
     }
@@ -18,6 +22,7 @@ public class PicoloBookService {
     }
 
     public static void setBook(PicoloBook book){
+        Log.d(TAG, "setting book");
         PicoloBookServiceHolder.INSTANCE.selectedBook = book;
     }
 }
