a551ff00f7a7c701c02dc262fdaa2eda5786c513
Vincent <vincent.dumont@isen.yncrea.fr>
2019-04-10 13:59:10 +0200

    travail sur activity image et activity video

--- app/src/main/java/com/example/picoloid/source/activity/MainActivity.java
+++ app/src/main/java/com/example/picoloid/source/activity/MainActivity.java
@@ -1,26 +1,12 @@
 package com.example.picoloid.source.activity;
 
 import android.content.Intent;
-import android.support.constraint.ConstraintLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 
 import com.example.picoloid.R;
-import com.example.picoloid.source.managerData.JsonManager;
-import com.example.picoloid.source.managerData.ObjectManager;
-import com.example.picoloid.source.model.PicoloBook;
-import com.example.picoloid.source.model.PicoloButton;
-import com.example.picoloid.source.service.PicoloBookService;
-import com.example.picoloid.source.util.PicoloBookTest;
-import com.example.picoloid.source.view.PicoloButtonView;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,26 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        try {
-            PicoloBookService.setBook(ObjectManager.loadBookAssetsmod(
-                    "Theo",
-                    this,
-                    "test"
-            ));
-            Button button = (Button)findViewById(R.id.openPageButton);
-            button.setOnClickListener(new View.OnClickListener() {
+        Button button = findViewById(R.id.openPageButton);
+        button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent ii=new Intent(MainActivity.this, PageActivityUser.class);
-                    ii.putExtra("pageId", 0);
+                    Intent ii=new Intent(MainActivity.this, ImageActivity.class);
                     startActivity(ii);
                 }
             });
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
     }
 }
