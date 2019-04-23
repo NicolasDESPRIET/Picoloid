6dc4faef848c6264c269661759b781d8243dd3b0
Roubaud Philémon <roubaudphilemon@gmail.com>
2019-04-23 14:01:44 +0200

    maj

--- app/src/main/java/com/example/picoloid/source/activity/ListPageActivity.java
+++ app/src/main/java/com/example/picoloid/source/activity/ListPageActivity.java
@@ -1,9 +1,11 @@
 package com.example.picoloid.source.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 
 import com.example.picoloid.R;
